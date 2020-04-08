857d79ce1675ce881cd1544e61a52765d962ef3a
Chaïma BEN YOUNES <benyounes@brocelia.fr>
2020-04-08 15:42:03 +0200

    [add] new entities and resource

---
+++ src/Entity/UserConfirmation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class UserConfirmation
+{
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(min=30, max=30)
+     */
+    public $confirmationToken;
+}
